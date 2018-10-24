d81bd1b5075d5a3b710eb71e571bc9eb8b8fa41a
Erwin Majewski <erwin.majewski@gmail.com>
2018-10-24 21:59:45 +0200

    Further JSP to Thymeleaf migration

--- target/gs-mysql-data/WEB-INF/classes/META-INF/resources/WEB-INF/jsp/js/app.js
+++
@@ -1,9 +0,0 @@
-function validate(id) {
-    var name = document.getElementById(id).value;
-    if (name == '') {
-        alert('Please enter a valid name.');
-        return false;
-    } else {
-        return true;
-    }
-}
